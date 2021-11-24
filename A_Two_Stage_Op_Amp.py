9d16b7fe58f48bf898f7f289b16c2224ffcd50a9
“Dignified-ghost” <junwei.work@qq.com>
2021-11-24 09:16:08 +0800

    A Two-Stage Op Amp calculating process

--- A_Two_Stage_Op_Amp.py
+++ A_Two_Stage_Op_Amp.py
@@ -1,4 +1,5 @@
 # Easy implementation just for calculating
+import math
 
 # known parameters
 K_n = 120 * 1e-6
@@ -24,25 +25,30 @@ P_diss = 2 * 1e-3
 
 # calculating process
 
-Cc = 2.2 * C_L
+Cc = (math.ceil(2.2) / 10) * C_L
 I5 = Cc * SR
-S3 = (0.5 * I5) / (2 * K_p * (VDD - Vout_max - (Vth_n + Vth_o) + (Vth_p - Vth_o)))
+S3 = (2 * 0.5 * I5) / (K_p * (VDD - Vout_max - (Vth_n + Vth_o) + (Vth_p - Vth_o)) ** 2)
+S3 = round(S3)
 S4 = S3
 
 # here not write p3
 gm3 = (2 * K_p * S3 * (0.5 * I5)) ** 0.5
 # here not write p3
 
-gm1 = GB * Cc
+gm1 = GB * Cc * 2 * math.pi
 S1 = gm1 ** 2 / (2 * K_n * (0.5 * I5))
+S1 = math.ceil(S1)
 S2 = S1
 
-Von5 = Vin_min - (2 * (0.5 * I5) / (K_n * S1)) - (Vth_n + Vth_o)
+Von5 = Vin_min - (2 * (0.5 * I5) / ((K_n * S1))) ** 0.5 - (Vth_n + Vth_o)
 S5 = 2 * I5 / (K_n * Von5 ** 2)
+S5 = math.ceil(S5)
 
 gm6 = 10 * gm1
 gm4 = gm3
 S6 = S4 * gm6 / gm4
+S6 = math.ceil(S6)
 
 I6 = gm6 ** 2 / (2 * K_p * S6)
-S7 = I6 / I5 * S6
+S7 = I6 / I5 * S5
+S7 = math.ceil(S7)
